a0305b6f5b751ad1132f6967b8a943f0b6cb12c7
Horace-BELOT <85863077+Horace-BELOT@users.noreply.github.com>
2025-03-24 16:55:30 +0100

    feat: multiprocess run

---
+++ 4process_4gpu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=gpu_optim_job
+#SBATCH --partition=unpreemptible
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-gpu=12      
+#SBATCH --mem=32G
+#SBATCH --output=/home/horace/data/code/test_optim_gpu/toy-example-gpu/_logs/log_%A_%a.out
+
+srun uv run /home/horace/data/code/test_optim_gpu/toy-example-gpu/examplegpu/src/main.py
