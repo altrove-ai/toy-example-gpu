2c7aee6c374a98fcbdc8019e364f7bae666f7707
Horace-BELOT <85863077+Horace-BELOT@users.noreply.github.com>
2025-03-24 18:25:26 +0100

    feat: testing sgd ^^

--- examplegpu/src/main.py
+++ examplegpu/src/main.py
@@ -38,12 +38,12 @@ def main() -> None:
     model = model.to(device)  # Move model to GPU
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adagrad(model.parameters(), lr=learning_rate)
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
 
     # Train the model
     wandb_config = {
         "learning_rate": learning_rate,
-        "optimizer": "adagrad",
+        "optimizer": "sgd",
     }
     run_name = f"run_{os.getenv('SLURM_JOB_ID')}_{os.getenv('SLURM_PROCID')}"
     with WandbLogger(
