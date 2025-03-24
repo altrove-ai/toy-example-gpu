43b5dee33acde800a5e701e273cfdf7acc8bf191
Horace-BELOT <85863077+Horace-BELOT@users.noreply.github.com>
2025-03-24 16:59:22 +0100

    feat: fixing cpu count per task

--- 4process_4gpu.sh
+++ 4process_4gpu.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=4
 #SBATCH --gpus-per-task=1
-#SBATCH --cpus-per-gpu=12      
+#SBATCH --cpus-per-gpu=4
 #SBATCH --mem=32G
 #SBATCH --output=/home/horace/data/code/test_optim_gpu/toy-example-gpu/_logs/log_%A_%a.out
 
