086358090f7444cacc98bfc76ed7446276ce0a05
Horace-BELOT <85863077+Horace-BELOT@users.noreply.github.com>
2025-03-25 12:04:09 +0100

    feat: azerazefs

--- 1process_4gpu_nsight.sh
+++ 1process_4gpu_nsight.sh
@@ -12,4 +12,4 @@
 PROFILE_OUTPUT="/home/horace/data/code/test_optim_gpu/toy-example-gpu/_logs/profile_result"
 
 # Start profiling using cudaProfilerApi (triggered by cudaProfilerStart/Stop in the application)
-nsys profile -c cudaProfilerApi --output=$PROFILE_OUTPUT --gpu-metrics-devices="cuda-visible" srun uv run /home/horace/data/code/test_optim_gpu/toy-example-gpu/examplegpu/src/main.py
+nsys profile -c none --output=$PROFILE_OUTPUT --gpu-metrics-devices="cuda-visible" srun uv run /home/horace/data/code/test_optim_gpu/toy-example-gpu/examplegpu/src/main.py
